f46ea56d411be8d472084a40e51987ab38bb1ca1
chaeha293@naver.com <chaeha293@naver.com>
2022-08-10 15:50:31 +0900

    #3.4 Falling in Love with Admin Panel

--- users/admin.py
+++ users/admin.py
@@ -2,6 +2,18 @@ from django.contrib import admin
 from . import models
 
 # Register your models here.
+# admin.site.register(models.User, CustomUserAdmin)
 @admin.register(models.User)
 class CustomUserAdmin(admin.ModelAdmin):
-    pass
+
+    """Custom User Admin"""
+
+    list_display = (
+        "username",
+        "email",
+        "gender",
+        "language",
+        "currency",
+        "superhost",
+    )
+    list_filter = ("language", "currency", "superhost")
