041c420a7a49667d0b38fbfbbe2ce19c85a59df6
chaeha293@naver.com <chaeha293@naver.com>
2022-08-19 11:17:07 +0900

    #4.5 Meta Class and Photos Model

--- core/models.py
+++ core/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
 class TimeStampedModel(models.Model):
 
     """Time Stamped model"""
 
-    created = models.DateTimeField()
-    updated = models.DateTimeField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
 
     class Meta:
         abstract = True  # 데이터베이스에 나타나지 않는 모델
--- users/admin.py
+++ users/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from . import models
 
+
 # Register your models here.
 # admin.site.register(models.User, CustomUserAdmin)
 @admin.register(models.User)
